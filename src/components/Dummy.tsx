103c7921e29706b73e8e0db2277ddd25ad08f75a
jam <jamhejratipro@gmail.com>
2023-12-08 18:43:28 +0330

    build: add nextui library

---
+++ src/components/Dummy.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@nextui-org/button";
+import React from "react";
+
+export default function Dummy() {
+  return (
+    <>
+      <div>Dummy</div>
+      <Button>Next Ui Button</Button>
+    </>
+  );
+}
