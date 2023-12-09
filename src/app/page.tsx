2924ac99e10c85bf368d75aecf856374645cca00
jam <jamhejratipro@gmail.com>
2023-12-09 22:46:21 +0330

    build: add jotai and configs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
-import Dummy from '@/components/Dummy'
-import Image from 'next/image'
+import Dummy from "@/components/Dummy";
+import { counterNumber } from "@/store/counter";
+import { Card, CardBody, Checkbox } from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <Dummy/>
-  )
+    <>
+      <Dummy />
+      <Card>
+        <CardBody>This is the Card body</CardBody>
+      </Card>
+      <Checkbox defaultSelected>Test</Checkbox>
+
+    </>
+  );
 }
--- src/components/Dummy.tsx
+++ src/components/Dummy.tsx
@@ -1,11 +1,18 @@
+"use client";
+
+import { counterNumber } from "@/store/counter";
 import { Button } from "@nextui-org/button";
+import { useAtom } from "jotai";
 import React from "react";
 
 export default function Dummy() {
+  const [counter, setCounter] = useAtom(counterNumber);
+
   return (
-    <>
-      <div>Dummy</div>
-      <Button>Next Ui Button</Button>
-    </>
+    <div className="my-2">
+      <div className="ml-12">counter: {counter}</div>
+      <Button className="mx-2" onClick={() => setCounter(counter + 1)}>Increase</Button>
+      <Button onClick={() => setCounter(counter - 1)}>Decrease</Button>
+    </div>
   );
 }
