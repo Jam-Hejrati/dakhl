13234d2eb5b98e14d5afbc7624453924d859224c
Jam <jamhejratipro@gmail.com>
2024-02-11 15:36:53 +0330

    feat: add profile section to header

--- src/components/Dashboard/Header/Profile/Profile.tsx
+++ src/components/Dashboard/Header/Profile/Profile.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image';
 export default function Profile() {
   return (
     <div>
-      <div className='m-1 flex flex-row-reverse items-center gap-2'>
-        <Image src={ProfileImage} style={{ borderRadius: '50%' }} width='40' height='40' alt='عکس پروفایل' />
-        <p className='text-lg font-semibold mb-1.5'>جم هجرتی</p>
+      <div className='m-2 flex flex-row-reverse items-center gap-2'>
+        <Image src={ProfileImage} style={{ borderRadius: '50%', width: '10%', maxWidth: '40px' }} alt='عکس پروفایل' />
+        <p className='text-base font-semibold mb-0.5 text-slate-800'>جم هجرتی</p>
       </div>
     </div>
   );
